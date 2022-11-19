32cd5cd8c7787a053f446617e2461c5b4882e58a
byronurrutia <b.urrutia@me.com>
2022-11-19 08:18:44 -0800

    comments added

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
-var number = "";
-var previousNumber = 0;
-var numbers = [];
-var operations = [];
+var number = ""; //stored pressed number button as string
+var numbers = []; //array of inputed numbers
+var operations = []; //array of inputed operations
 const text = document.getElementById("result-text");
 
 function recordNumber(n) {
+  //when the user has just caculated a equation but wants to get rid of the result
+  //clear the history of numbers
   if (operations.length === 0) {
     numbers = [];
   }
+  //store and add the strings together
+  //add digit
   number += n;
   updateDisplay(number);
 }
 
 function recordOperation(operation) {
+  //when the user has recored a number (result or regular input) and has inputed a current number
+  //record operation and current inputed number and clear the current number
   if (numbers.length >= 0 && number !== "") {
     operations.push(operation);
     numbers.push(Number(number));
     number = "";
-  } else if (numbers.length === 1 && number === "") {
+  }
+  //when the user wants to reuse the result of a previous calculation
+  //record the operation
+  else if (numbers.length === 1 && number === "") {
     operations.push(operation);
   }
-
+  //when the user has inputed an operation at least once, display the result after every operation press
   if (operations.length > 1) {
     updateDisplay(calculate());
   }
 }
 
 function clear() {
+  //when the use has a current number they want to clear
+  //clear number
   if ((number !== "" && operations.length > 0) || operations.length === 0) {
     number = "";
-  } else if (number === "") {
+  }
+  //when the user wants to clear everything
+  //clear history of numbers and operations
+  else if (number === "") {
     numbers = [];
     operations = [];
   }
@@ -37,9 +50,12 @@ function clear() {
 }
 
 function backspace() {
+  //when the user wants to remove the last digit they inputed
+  //delete last digit inputed
   if (number !== "") {
     number = number.slice(0, -1);
     updateDisplay(number);
+    //when the user has deleted a digit and there is no more to delete
     if (number === "") {
       updateDisplay(0);
     }
@@ -48,23 +64,33 @@ function backspace() {
 
 function equals() {
   let result = 0;
+  //if the user pressed the equal with a current number stored
+  //record number and clear the current number
   if (number !== "") {
     numbers.push(Number(number));
     number = "";
+
+    //when the user pressed the equals with no current number stored
+    //push the latest number from the history into the numbers array
   } else if (number === "") {
     numbers.push(numbers[numbers.length - 1]);
   }
 
+  //calculate the current sum with appropriate operations
   result = calculate();
   updateDisplay(result);
+  //clear operation and number history
   operations = [];
   numbers = [];
+  //push the calcuated result in the number array
   numbers.push(result);
 }
 
 function calculate() {
+  //copy the numbers array and operations array
   let operationsCopy = [...operations];
   let numbersCopy = [...numbers];
+  //perform a summation depending on the operation in each operation array index
   let result = numbersCopy.reduce((accumulator, currentValue) => {
     if (operationsCopy[0] === "/") {
       operationsCopy.shift();
@@ -143,6 +169,7 @@ document.getElementById("backspace-btn").addEventListener("click", backspace);
 document.getElementById("equal-equa_btn").addEventListener("click", equals);
 
 // keyboard input
+// recognize only desired inputs, ignore the rest
 addEventListener("keydown", (event) => {
   if (Number(event.key) >= 0 && Number(event.key) < 10) {
     recordNumber(event.key);
